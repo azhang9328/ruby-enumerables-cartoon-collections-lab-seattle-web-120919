26c8db9687a6fd60849c4f6985abafe81e9ff522
Allen Zhang <azhang9328@gmail.com>
2019-12-06 01:56:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,9 +13,7 @@ def summon_captain_planet(veggies)
 end
 
 def long_planeteer_calls(calls_long)
-  calls_long.reduce(nil) do |total, word|
-    total + word
-  end 
+
 end
 
 def find_the_cheese# code an argument here
