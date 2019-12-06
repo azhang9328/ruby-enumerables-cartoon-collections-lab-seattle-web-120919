3d07212eb24b1dc83247009edec343e8f768729b
Allen Zhang <azhang9328@gmail.com>
2019-12-06 01:18:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,9 @@ def summon_captain_planet(veggies)
 end
 
 def long_planeteer_calls(calls_long)
-  calls_long.reduce do |
+  calls_long.reduce do 
+    
+  end 
 end
 
 def find_the_cheese# code an argument here
