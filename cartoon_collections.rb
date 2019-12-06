b9fe0a4e48405ed24f831f84037fe2cd5d1ea3d7
Allen Zhang <azhang9328@gmail.com>
2019-12-06 01:49:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,11 +14,7 @@ end
 
 def long_planeteer_calls(calls_long)
   calls_long.reduce(total = nil) do |total, word|
-    if calls_long.length > 4
-      puts calls_long
-      puts "true"
-      true 
-    end
+    puts calls_long
   end 
 end
 
