df47719d6260c54930e63a5d0a75beaf85d686ee
Allen Zhang <azhang9328@gmail.com>
2019-12-06 02:40:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,18 +22,9 @@ def long_planeteer_calls(calls_long)
   return false
 end
 
-def find_the_cheese(countains_cheddar)
+def find_the_cheese(contains_cheddar)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese_types.map do |cheese_types| 
-    if countains_cheddar.include?(cheese_types)
-      countains_cheddar.map do |countains_cheddar|
-        if countains_cheddar == cheese_types
-          puts countains_cheddar
-          return countains_cheddar
-        end   
-      end   
-    else   
-      return nil
-    end 
-  end 
+  contains_cheddar.map do |contains_cheddar|
+    
+  end   
 end
